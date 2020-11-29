2b0f538bf1d34eef2e897ea85687028b776e8e44
Brian Pepple <bdpepple@gmail.com>
2020-11-29 13:38:26 -0500

    Format with black

--- open_video_db/wsgi.py
+++ open_video_db/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'open_video_db.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "open_video_db.settings")
 
 application = get_wsgi_application()
